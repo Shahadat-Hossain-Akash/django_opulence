15c6b4e2dbb78118b32dad11adbde05fbef7ec4c
Shahadat <shahadat.akash007@gmail.com>
2024-02-29 00:13:03 +0600

    minor fixes

--- backend/settings.py
+++ backend/settings.py
@@ -119,12 +119,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
