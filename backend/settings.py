13c49f8586ff0f5efeea761725242eb758a2a45f
Shahadat <shahadat.akash007@gmail.com>
2024-03-14 02:01:37 +0600

    cors settings updated

--- backend/settings.py
+++ backend/settings.py
@@ -17,6 +17,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['.now.sh', '.vercel.app', '127.0.0.1', 'localhost']
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+CORS_ALLOW_CREDENTIALS = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -33,9 +38,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
